a2d37a9ee438eea33576ffafe9a00c79e2366973
Josh Charpentier <charpentier.josh@gmail.com>
2024-12-24 15:00:10 -0600

    Initial Commit

---
+++ ElementalMatcher/ElementalMatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElementalMatcherApp.swift
+//  ElementalMatcher
+//
+//  Created by Josh Charpentier on 12/24/24.
+//
+
+import SwiftUI
+
+@main
+struct ElementalMatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
